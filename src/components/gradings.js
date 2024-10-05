83e0ad9ded1feaeea918cf9d5804d44e4a3747d2
Pasindu Kavinda <pasindu2021kavinda@gmail.com>
2024-10-05 21:41:05 +0530

    quizes and tests interface done

---
+++ src/components/gradings.js
@@ -0,0 +1,170 @@
+import React, { useState } from 'react'
+
+export default function Gradings() {
+
+    const [active, setactive] = useState(1)
+    return (
+        <div>
+
+            <div className='w-full bg-blue-600 text-center text-white rounded-lg'>
+                <div className={`${active === 1 ? "bg-blue-800" : "bg-blue-600"} px-12 py-3 cursor-pointer inline-block hover:bg-blue-700 duration-200`} onClick={() => setactive(1)}>Quiz Marks</div>
+                <div className={`${active === 2 ? "bg-blue-800" : "bg-blue-600"} px-12 py-3 cursor-pointer inline-block hover:bg-blue-700 duration-200`} onClick={() => setactive(2)}>Module Test Marks</div>
+                <div className={`${active === 3 ? "bg-blue-800" : "bg-blue-600"} px-12 py-3 cursor-pointer inline-block hover:bg-blue-700 duration-200`} onClick={() => setactive(3)}>Target Question Marks</div>
+                <div className={`${active === 4 ? "bg-blue-800" : "bg-blue-600"} px-12 py-3 cursor-pointer inline-block hover:bg-blue-700 duration-200`} onClick={() => setactive(4)}>Paper Marks</div>
+            </div>
+
+            <div className={`${active === 1 ? "block" : "hidden"}`}>
+                <div className='py-5 bg-purple-500 mt-10 grid grid-cols-2 px-12 text-white rounded-lg'>
+                    <div className='text-2xl'>Overall Quiz Marks : </div>
+                    <div className='text-3xl text-right'>92%</div>
+                </div>
+                <div className='h-c500 w-full overflow-auto'>
+                    <table className='w-full mt-5'>
+                        <tr className='bg-gray-300'>
+                            <td className='py-2 px-5 text-left'>Quiz Name</td>
+                            <td className='py-2 px-5 '>Quiz ID</td>
+                            <td className='py-2 px-5 '>Marks</td>
+                        </tr>
+
+                        |{(() => {
+                            const arr = []
+
+                            for (let i = 0; i < 6; i++) {
+                                arr.push(
+                                    <tr className='mt-2'>
+                                        <td className='py-2 px-5 text-left'>Module Quiz</td>
+                                        <td className='py-2 px-5 '>1001 - {i + 1}</td>
+                                        <td className='py-2 px-5 '>{15 * i + 5}%</td>
+                                    </tr>
+                                )
+
+                            }
+
+                            return arr
+                        })()}
+                    </table>
+                </div>
+
+
+            </div>
+
+
+
+
+
+
+            <div className={`${active === 2 ? "block" : "hidden"}`}>
+                <div className='py-5 bg-purple-500 mt-10 grid grid-cols-2 px-12 text-white rounded-lg'>
+                    <div className='text-2xl'>Overall Module Test Marks : </div>
+                    <div className='text-3xl text-right'>79%</div>
+                </div>
+                <div className='h-c500 w-full overflow-auto'>
+                    <table className='w-full mt-5'>
+                        <tr className='bg-gray-300'>
+                            <td className='py-2 px-5 text-left'>Module Test Name</td>
+                            <td className='py-2 px-5 '>Module test ID</td>
+                            <td className='py-2 px-5 '>Marks</td>
+                        </tr>
+
+                        |{(() => {
+                            const arr = []
+
+                            for (let i = 0; i < 6; i++) {
+                                arr.push(
+                                    <tr className='mt-2'>
+                                        <td className='py-2 px-5 text-left'>Module test</td>
+                                        <td className='py-2 px-5 '>M1001 - {i + 1}</td>
+                                        <td className='py-2 px-5 '>{12 * i + 5}%</td>
+                                    </tr>
+                                )
+
+                            }
+
+                            return arr
+                        })()}
+                    </table>
+                </div>
+
+
+            </div>
+
+
+
+
+
+            <div className={`${active === 3 ? "block" : "hidden"}`}>
+                <div className='py-5 bg-purple-500 mt-10 grid grid-cols-2 px-12 text-white rounded-lg'>
+                    <div className='text-2xl'>Overall Target Question Marks : </div>
+                    <div className='text-3xl text-right'>85%</div>
+                </div>
+                <div className='h-c500 w-full overflow-auto'>
+                    <table className='w-full mt-5'>
+                        <tr className='bg-gray-300'>
+                            <td className='py-2 px-5 text-left'>Target Question Name</td>
+                            <td className='py-2 px-5 '>Target Question ID</td>
+                            <td className='py-2 px-5 '>Marks</td>
+                        </tr>
+
+                        |{(() => {
+                            const arr = []
+
+                            for (let i = 0; i < 6; i++) {
+                                arr.push(
+                                    <tr className='mt-2'>
+                                        <td className='py-2 px-5 text-left'>Target Questions</td>
+                                        <td className='py-2 px-5 '>TQ100{i + 1}</td>
+                                        <td className='py-2 px-5 '>{14 * i + 5}%</td>
+                                    </tr>
+                                )
+
+                            }
+
+                            return arr
+                        })()}
+                    </table>
+                </div>
+
+
+            </div>
+
+
+
+
+
+            <div className={`${active === 4 ? "block" : "hidden"}`}>
+                <div className='py-5 bg-purple-500 mt-10 grid grid-cols-2 px-12 text-white rounded-lg'>
+                    <div className='text-2xl'>Overall Paper Marks : </div>
+                    <div className='text-3xl text-right'>74%</div>
+                </div>
+                <div className='h-c500 w-full overflow-auto'>
+                    <table className='w-full mt-5'>
+                        <tr className='bg-gray-300'>
+                            <td className='py-2 px-5 text-left'>Paper Name</td>
+                            <td className='py-2 px-5 '>Paper No</td>
+                            <td className='py-2 px-5 '>Marks</td>
+                        </tr>
+
+                        |{(() => {
+                            const arr = []
+
+                            for (let i = 0; i < 6; i++) {
+                                arr.push(
+                                    <tr className='mt-2'>
+                                        <td className='py-2 px-5 text-left'>Full Paper</td>
+                                        <td className='py-2 px-5 '>0{i + 1}</td>
+                                        <td className='py-2 px-5 '>{16 * i + 5}%</td>
+                                    </tr>
+                                )
+
+                            }
+
+                            return arr
+                        })()}
+                    </table>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
--- src/components/quizs.js
+++ src/components/quizs.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import Gradings from './gradings'
+
+
 
 export default function Quizs() {
     const [active, setactive] = useState(1)
@@ -18,17 +21,16 @@ export default function Quizs() {
 
                 <div className='w-cw80 h-c750 bg-slate-100 rounded-lg'>
 
-                    <div className={`${active === 1 ? "block" : "hidden"}  px-2 py-4 h-c400`}>
+                    <div className={`${active === 1 ? "block" : "hidden"} px-2 py-4 h-c400`}>
                         <div className='overflow-auto h-c400'>
                             {(() => {
                                 const arr = []
                                 for (let i = 0; i < 5; i++) {
                                     arr.push(
-                                        <div className=' bg-blue-100 px-10 py-5 rounded-lg mb-5'>
+                                        <div key={i} className='bg-blue-100 px-10 py-5 rounded-lg mb-5'>
                                             <div className='font-csans text-2xl'>Title : Module Quiz</div>
                                             <div className='grid grid-cols-3'>
                                                 <div>
-
                                                     <div className='font-bold text-xl pt-5'>Coverings :</div>
                                                     <div className='pl-12'>
                                                         <div>Try to answer all Questions</div>
@@ -36,14 +38,15 @@ export default function Quizs() {
                                                     </div>
                                                 </div>
                                                 <div className='pt-5'>
-                                                    <div className='pt-3 font-bold'>QUIZ ID : 1001-1</div>
+                                                    <div className='pt-3 font-bold'>QUIZ ID : 1001-{i + 1}</div>
                                                     <div>Total Questions  : 20</div>
-                                                    <div>Time Allowd : 1hr</div>
-                                                    <div className='text-red-600'>Status : Not Compleated</div>
+                                                    <div>Time Allowed : 1hr</div>
+                                                    <div className='text-red-600'>Status : Not Completed</div>
                                                 </div>
-
-                                                <div >
-                                                    <div className='py-2 px-5 bg-blue-600 rounded-lg hover:scale-105 duration-300 w-fit text-white mt-10'>Answer Quiz</div>
+                                                <div>
+                                                    <div className='py-2 px-5 bg-blue-600 rounded-lg hover:scale-105 duration-300 w-fit text-white mt-10'>
+                                                        Answer Quiz
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -53,6 +56,11 @@ export default function Quizs() {
                             })()}
                         </div>
                     </div>
+
+
+
+
+
 
 
                     <div className={`${active === 2 ? "block" : "hidden"} px-2 py-2 h-c400 ove`}>
@@ -107,7 +115,118 @@ export default function Quizs() {
 
 
 
+
+
+
+                    <div className={`${active === 3 ? "block" : "hidden"} px-2 py-2 h-c400 ove`}>
+                        <div className='py-2 px-2 grid grid-cols-2 bg-blue-600 rounded-lg'>
+                            <div className='text-xl text-white px-12 py-5'>Select Your Module Number</div>
+                            <div className='text-xl text-white  px-10 py-5'>
+                                <select name="cars" id="cars" className='bg-blue-500 py-1 px-5 rounded-lg'>
+                                    <option value="volvo">Lesson 01</option>
+                                    <option value="volvo">Lesson 02</option>
+                                    <option value="volvo">Lesson 03</option>
+                                    <option value="volvo">Lesson 04</option>
+                                    <option value="volvo">Lesson 05</option>
+                                    <option value="volvo">Lesson 06</option>
+                                    <option value="volvo">Lesson 07</option>
+                                    <option value="volvo">Lesson 08</option>
+                                    <option value="volvo">Lesson 09</option>
+                                    <option value="volvo">Lesson 10</option>
+                                    <option value="volvo">Lesson 11</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className='mt-5 h-c600 overflow-auto'>
+                            {(() => {
+                                const arr = []
+                                for (let i = 0; i < 5; i++) {
+                                    arr.push(
+                                        <div className='mb-5 w-full px-2 py-4 rounded-lg bg-blue-100 h-c700 overflow-auto'>
+                                            <div className='text-xl font-bold'>Target Question ID : TQ100{i + 1}</div>
+                                            <div className='grid grid-cols-2'>
+                                                <div className='pl-12 pt-5'>
+                                                    <div className='font-semibold text-lg'>Instriuctions.</div>
+                                                    <div> - Answer in a Seperate paper clearly.</div>
+                                                    <div> - on that paper clearly state your index no, name , a/l year and hall location</div>
+                                                    <div> - using a scannar or your mobile and create a pdf file of that answered file</div>
+                                                    <div> - then upload it into corresponding Upload link at ADD SUBMISSION page</div>
+                                                </div>
+
+                                                <div className='text-lg text-white py-2 px-10 bg-blue-600 rounded-lg hover:scale-105 duration-300 w-fit h-fit'>Download Paper</div>
+
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                                return arr
+                            })()}
+
+                        </div>
+
+                    </div>
+
+
+
+
+
+
+
+
+                    <div className={`${active === 4 ? "block" : "hidden"}  px-2 py-4 h-c400`}>
+                        <div className='overflow-auto h-c400'>
+                            {(() => {
+                                const arr = []
+                                for (let i = 0; i < 5; i++) {
+                                    arr.push(
+                                        <div className=' bg-blue-100 px-10 py-5 rounded-lg mb-5'>
+                                            <div className='font-csans text-2xl'>Title : Test Paper {i + 1}</div>
+                                            <div className=''>
+                                                <div>
+
+                                                    <div className='font-bold text-xl pt-5'>Instructions :</div>
+                                                    <div className='pl-12 pt-5'>
+                                                        <div className='font-semibold text-lg'>Instriuctions.</div>
+                                                        <div> - Answer in a Seperate paper clearly.</div>
+                                                        <div> - on that paper clearly state your index no, name , a/l year and hall location</div>
+                                                        <div> - using a scannar or your mobile and create a pdf file of that answered file</div>
+                                                        <div> - then upload it into corresponding Upload link at ADD SUBMISSION page</div>
+                                                    </div>
+                                                </div>
+
+
+                                                <div >
+                                                    <div className='py-2 px-5 bg-blue-600 rounded-lg hover:scale-105 duration-300 w-fit text-white mt-10'>Download Paper</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                                return arr
+                            })()}
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+
+                    <div className={`${active === 5 ? "block" : "hidden"}  px-2 py-4 h-c400`}>
+                        <Gradings />
+                    </div>
+
                 </div>
+
+
+
+
+
+
             </div>
         </div>
     )
